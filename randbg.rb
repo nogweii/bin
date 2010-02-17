265162105b448d671f00209263a3db5b822ac625
Colin Shea <colin@evaryont.me>
2010-02-16 21:01:58 -0500

    Deleted old stuffs.

--- randbg.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-
-# Given a preconfigured path (or the environment variable BACKGROUNDS),
-# recursively scans through the directory for all jpegs and pngs and randomly
-# selects one. Then call `feh --xinerama --bg-scale <file>`
