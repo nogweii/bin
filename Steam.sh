a1937cb9f52b7d774368ffcdecfdc4b1b0d7d16e
Colin Shea <colin@evaryont.me>
2010-02-17 18:08:53 -0500

    Delete old stuff, part #3.

--- Steam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# WINE binary
-CDLOADER_WINE="wine"
-
-WINEDEBUG="-all" nice -n -8 $CDLOADER_WINE "C:/Program Files/Steam/steam.exe" -- "$@"
--- VirtualBox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export VBOX_ALSA_ADC_DEV="hw:0,0"
-exec VirtualBox
