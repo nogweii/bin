590a086928d8c5b0764cbe59e718158e963300a6
Colin Shea <colin@evaryont.me>
2010-05-02 20:55:40 -0400

    New script: dmenu-playlist.sh
    Expanded form of a one-liner found on the MPD wiki.

---
+++ dmenu-playlist.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Open DMenu with each item, or if the user doesn't choose a song, output ';;;'
+song_choice=`(mpc playlist | dmenu -i -p 'song name') || echo ";;;"`
+
+# List all the songs, prepend a number then a semicolon
+# Then search for the song the user choose earlier, and output it's number
+song_number=`mpc playlist | nl -s ';' | sed -n "s@^ *\([0-9]\+\);$song_choice@\1@p"`
+
+# Play that position in the playlist
+mpc play $song_number
